a2504ab90f5ebf89390f7eeeb89d45ded85c924d
Mag Mell <magmell@oss.cipunited.com>
2026-09-01 18:49:55 +0800

    chore: correct copyright headers (#19)
    Use the actual project year (2026, first commit 2026-08-21) and
    the AOSC-Dev org name instead of "AOSC Updates contributors".

--- src/amoupdates.cpp
+++ src/amoupdates.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2024 by AOSC Updates contributors                       *
+ *   Copyright (C) 2026 by AOSC-Dev                                        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
